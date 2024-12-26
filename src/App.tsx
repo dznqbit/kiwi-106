e6018d8346048b0b0e5c96221e94325d0c558190
Quinton Harris <quinton.harris@gmail.com>
2024-12-25 22:51:41 -0800

    Add prettier, lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,49 @@
-import { useEffect, useState } from 'react'
-import { WebMidi } from 'webmidi'
-import './App.css'
-
+import { useEffect } from "react";
+import { WebMidi } from "webmidi";
+import "./App.css";
 
 function onEnabled() {
   // Inputs
-  WebMidi.inputs.forEach(input => console.log(input.manufacturer, input.name));
-  
+  WebMidi.inputs.forEach((input) =>
+    console.log(input.manufacturer, input.name),
+  );
+
   // Outputs
-  WebMidi.outputs.forEach(output => console.log(output.manufacturer, output.name));
+  WebMidi.outputs.forEach((output) =>
+    console.log(output.manufacturer, output.name),
+  );
 }
 
 function App() {
   useEffect(() => {
-    WebMidi
-      .enable()
+    WebMidi.enable()
       .then(onEnabled)
-      .catch(err => alert(err));
+      .catch((err) => alert(err));
   }, []);
 
   const deviceName = "mioXC";
 
   const noteOn = () => {
-    const myOutput = WebMidi.getOutputByName(deviceName);
-    let output = WebMidi.outputs[0];
-    let channel = output.channels[1];
+    const output = WebMidi.getOutputByName(deviceName);
+    const channel = output.channels[1];
     channel.playNote("C3");
-  }
+  };
 
-  const noteOff = ()  => {
+  const noteOff = () => {
     const myOutput = WebMidi.getOutputByName(deviceName);
     // Panic
     myOutput.sendAllSoundOff();
-  }
+  };
 
   return (
     <>
       <h1>Kiwi 106 Programmer</h1>
       <div className="card">
-        <button onClick={noteOn}>
-          Note On
-        </button>
-        <button onClick={noteOff}>
-          Note Off
-        </button>
+        <button onClick={noteOn}>Note On</button>
+        <button onClick={noteOff}>Note Off</button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
